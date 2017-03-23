0d884197beb943f9f3f098192d2be6e92f8db3af
Lajos Gallay <gallayl@sensenet.com>
2017-03-23 11:32:34 +0100

    feat(sn-fetch-types): Added Basic auth. and ZIP download functionality to sn-fetch-types command
    Added basic auth, download binary and open ZIP file functionality to sn-fetch-types command

--- bin/sn-fetch-types.ts
+++ bin/sn-fetch-types.ts
@@ -1,6 +1,10 @@
 import * as Path from 'path';
 import * as Prompt from 'prompt';
 import * as FileSystem from 'fs';
+import * as Http from 'http';
+import * as File from 'fs';
+import * as Stream from 'stream';
+import * as AdmZip from 'adm-zip';
 
 class SnConfig {
     RepositoryUrl: string;
@@ -8,7 +12,9 @@ class SnConfig {
     Password?: string;
 }
 
-const REPOSITORY_URL_POSTFIX = 'Root/System/Schema/Metadata/TypeScript/meta.zip'
+const SN_REPOSITORY_URL_POSTFIX = '/Root/System/Schema/Metadata/TypeScript/meta.zip'
+const SN_CONFIG_NAME = 'sn.config.js';
+const SN_TEMPDIR_NAME = 'sntmp';
 
 class Config {
 
@@ -16,9 +22,10 @@ class Config {
         let cfg: SnConfig;
 
         try {
-            cfg = require(cwd + Path.sep + 'sn.config.js');
+            cfg = require(cwd + Path.sep + SN_CONFIG_NAME);
         } catch (error) {
-            console.log('There was an error opening sn.config.js: ', error);
+            console.log(`No '${SN_CONFIG_NAME}' file found in the project root.`);
+            cfg = new SnConfig();
         }
         return cfg;
     }
@@ -54,6 +61,46 @@ class Config {
     }
 }
 
+class TypeDownloader {
+    public static async Fetch(cfg: SnConfig): Promise<Buffer> {
+        return new Promise<Buffer>((resolve, reject) => {
+            let filePath = `./${SN_TEMPDIR_NAME}/meta.zip`;
+            let auth = 'Basic ' + new Buffer(`${cfg.UserName}:${cfg.Password}`).toString('base64');
+            let headers = {
+                'Authorization': auth,
+            };
+            Http.get({
+                host: cfg.RepositoryUrl,
+                path: SN_REPOSITORY_URL_POSTFIX,
+                headers: headers,
+            }, (response: Http.IncomingMessage) => {
+
+                if (response.readable) {
+                    let data = [];
+                    let contentLength: number = parseInt(response.headers['content-length']);
+                    response.on('data', chunk => {
+                        data.push(chunk);
+                    })
+
+                    response.on('end', () => {
+                        let pos = 0;
+                        let buffer = new Buffer(contentLength);
+                        data.forEach(chunk => {
+                            chunk.copy(buffer, pos);
+                            pos += chunk.length;
+                        });
+                        resolve(buffer);
+                    });
+                }
+                else {
+                    reject();
+                }
+            })
+        });
+    }
+
+}
+
 console.log('Sn-Fetch-Types starting...');
 
 let path = Path.parse(__dirname);
@@ -62,10 +109,10 @@ let cwd = process.cwd();
 console.log(`The current working directory is: ${cwd}`);
 console.log('Reading Config file...');
 
+let cfg = Config.Create().then(async cfg => {
+    let zipBuffer = await TypeDownloader.Fetch(cfg);
 
-/* ToDo: enter manual data */
+    let zip = new AdmZip(zipBuffer);
 
-let cfg = Config.Create().then(cfg => {
-    console.log(cfg);
     process.exit(0);
 });
--- dist/bin/sn-fetch-types.js
+++ dist/bin/sn-fetch-types.js
@@ -10,18 +10,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const Path = require("path");
 const Prompt = require("prompt");
+const Http = require("http");
+const AdmZip = require("adm-zip");
 class SnConfig {
 }
-const REPOSITORY_URL_POSTFIX = 'Root/System/Schema/Metadata/TypeScript/meta.zip';
+const SN_REPOSITORY_URL_POSTFIX = '/Root/System/Schema/Metadata/TypeScript/meta.zip';
+const SN_CONFIG_NAME = 'sn.config.js';
+const SN_TEMPDIR_NAME = 'sntmp';
 class Config {
     static ReadConfigFile() {
         return __awaiter(this, void 0, void 0, function* () {
             let cfg;
             try {
-                cfg = require(cwd + Path.sep + 'sn.config.js');
+                cfg = require(cwd + Path.sep + SN_CONFIG_NAME);
             }
             catch (error) {
-                console.log('There was an error opening sn.config.js: ', error);
+                console.log(`No '${SN_CONFIG_NAME}' file found in the project root.`);
+                cfg = new SnConfig();
             }
             return cfg;
         });
@@ -58,14 +63,53 @@ class Config {
         });
     }
 }
+class TypeDownloader {
+    static Fetch(cfg) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return new Promise((resolve, reject) => {
+                let filePath = `./${SN_TEMPDIR_NAME}/meta.zip`;
+                let auth = 'Basic ' + new Buffer(`${cfg.UserName}:${cfg.Password}`).toString('base64');
+                let headers = {
+                    'Authorization': auth,
+                };
+                Http.get({
+                    host: cfg.RepositoryUrl,
+                    path: SN_REPOSITORY_URL_POSTFIX,
+                    headers: headers,
+                }, (response) => {
+                    if (response.readable) {
+                        let data = [];
+                        let contentLength = parseInt(response.headers['content-length']);
+                        response.on('data', chunk => {
+                            data.push(chunk);
+                        });
+                        response.on('end', () => {
+                            let pos = 0;
+                            let buffer = new Buffer(contentLength);
+                            data.forEach(chunk => {
+                                chunk.copy(buffer, pos);
+                                pos += chunk.length;
+                            });
+                            resolve(buffer);
+                        });
+                    }
+                    else {
+                        reject();
+                    }
+                });
+            });
+        });
+    }
+}
 console.log('Sn-Fetch-Types starting...');
 let path = Path.parse(__dirname);
 let cwd = process.cwd();
 console.log(`The current working directory is: ${cwd}`);
 console.log('Reading Config file...');
-let cfg = Config.Create().then(cfg => {
-    console.log(cfg);
+let cfg = Config.Create().then((cfg) => __awaiter(this, void 0, void 0, function* () {
+    let zipBuffer = yield TypeDownloader.Fetch(cfg);
+    let zip = new AdmZip(zipBuffer);
     process.exit(0);
-});
+}));
 
 //# sourceMappingURL=sn-fetch-types.js.map
