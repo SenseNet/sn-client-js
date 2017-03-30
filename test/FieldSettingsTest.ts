324d10e702ce57b5137044d73697e94308340d66
Lajos Gallay <gallayl@sensenet.com>
2017-03-30 12:35:21 +0200

    fix(FieldSettingsTests): Removed Sinon references

--- test/FieldSettingsTest.ts
+++ test/FieldSettingsTest.ts
@@ -1,39 +1,43 @@
 import { FieldSettings } from '../src/FieldSettings'
 import * as Chai from 'chai';
-import * as sinon from 'sinon';
 const expect = Chai.expect;
 
 describe('FieldSettings', () => {
     describe('#FieldSetting constructor', function () {
-        const fieldSetting = new FieldSettings.FieldSetting({ name: 'ShortText' });
+
+        let fieldSetting: FieldSettings.FieldSetting;
+        beforeEach(function () {
+            fieldSetting = new FieldSettings.FieldSetting({ name: 'ShortText' });
+        });
+
         it('should return a FieldSetting object', function () {
             expect(fieldSetting).to.be.an.instanceof(FieldSettings.FieldSetting);
         });
-        it('should return a FieldSetting object with a name Shorttext', function () {
-            expect(fieldSetting.Name).to.be.eq('Shorttext');
+        it('should return a FieldSetting object with a name ShortText', function () {
+            expect(fieldSetting.Name).to.be.eq('ShortText');
         });
         it('should return null as the Icon of the FieldSetting object', function () {
-            let spy = sinon.spy(FieldSettings, 'FieldSetting');
-            let MyClass = FieldSettings.FieldSetting;
-            let inst = new MyClass({ name: 'ShortText' });
-            expect(spy.calledOnce).to.be.true;
-            expect(spy.called).to.be.equal(true);
+            let inst = new FieldSettings.FieldSetting({ name: 'ShortText' });
+            expect(inst.Icon).to.be.eq(undefined);
         });
         it('returns the name', () => {
-            fieldSetting.Name.should.equal('ShortText');
+            expect(fieldSetting.Name).to.be.eq('ShortText');
         });
         it('can be changed', () => {
             fieldSetting.Name = 'Number';
-            fieldSetting.Name.should.equal('Number');
+            expect(fieldSetting.Name).to.be.equal('Number');
         });
-        it('should return a FieldSetting object with a name Shorttext', function () {
-            let obj1 = JSON.stringify({ Name: 'ShortText', DisplayName: null });
-            expect(JSON.stringify(fieldSetting)).to.be.eq(obj1);
+        it('should return a FieldSetting object with a name ShortText', function () {
+            let obj1 = JSON.stringify({ Name: 'ShortText' });
+            let obj2 = new FieldSettings.FieldSetting({
+                name: 'ShortText',
+            });
+            expect(obj1).to.be.eq(JSON.stringify(obj2));
         });
-        it('should return a FieldSetting object with a name Shorttext', function () {
+        it('should return a FieldSetting object with a name ShortText2', function () {
 
-            const fieldSetting = new FieldSettings.FieldSetting({ name: 'ShortText', displayName: 'AAA' });
-            let obj1 = JSON.stringify({ Name: 'ShortText', DisplayName: 'AAA' });
+            const fieldSetting = new FieldSettings.FieldSetting({ name: 'ShortText2', displayName: 'AAA' });
+            let obj1 = JSON.stringify({ Name: 'ShortText2', DisplayName: 'AAA' });
             expect(JSON.stringify(fieldSetting)).to.be.eq(obj1);
         });
     });
@@ -42,19 +46,17 @@ describe('FieldSettings', () => {
         it('should return a TextFieldSetting object', function () {
             expect(fieldSetting).to.be.an.instanceof(FieldSettings.TextFieldSetting);
         });
-        it('should return a TextFieldSetting object with a name Shorttext', function () {
-            expect(fieldSetting.Name).to.be.eq('Shorttext');
+        it('should return a TextFieldSetting object with a name ShortText', function () {
+            expect(fieldSetting.Name).to.be.eq('ShortText');
         });
         it('should return a TextFieldSetting object with the given params', function () {
             const obj1 = JSON.stringify(fieldSetting);
-            const obj2 = JSON.stringify({ name: 'ShortText', displayName: 'ShortText', maxLength: 2, minLength: 0 })
+            const obj2 = JSON.stringify({ Name: 'ShortText', DisplayName: 'ShortText', MinLength: 0, MaxLength: 2 })
             expect(obj1).to.be.eq(obj2);
         });
         it('should return null as the Icon of the TextFieldSetting object', function () {
-            let spy = sinon.spy(FieldSettings, 'TextFieldSetting');
-            let MyClass = FieldSettings.TextFieldSetting;
-            let inst = new MyClass({ name: 'ShortText' });
-            expect(spy.calledOnce).to.be.true;
+            let inst = new FieldSettings.TextFieldSetting({ name: 'ShortText' });
+            expect(inst.Icon).to.be.eq(undefined);
         });
     });
     describe('#ShortText', function () {
