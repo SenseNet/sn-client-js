e33897a7793aab73f803c5a8a72850836c816196
Lajos Gallay <gallayl@sensenet.com>
2017-04-04 14:29:55 +0200

    refactor(commands): removed sn-fetch-type command

--- bin/sn-fetch-types.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\..\sn-client-js\dist_commands\sn-fetch-types.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\..\sn-client-js\dist_commands\sn-fetch-types.js" %*
-)
