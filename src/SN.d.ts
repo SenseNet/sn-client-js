186170640470492cab6d8b4d3f2de45fa8c7af68
gallayl <gallay.lajos@gmail.com>
2017-05-05 13:08:30 +0200

    feat(ODataApiActionObservables): removed ODataApiActionObservables (sn-redux will use Repository)

--- src/SN.d.ts
+++ src/SN.d.ts
@@ -8,7 +8,6 @@ import * as Schemas from './Schemas';
 import * as Enums from './Enums';
 import * as Collection from './Collection';
 import * as ODataHelper from './ODataHelper';
-import * as ODataAPIActionObservables from './ODataAPIActionObservables';
 import * as ODataApi from './ODataApi';
 import * as Resources from './Resources';
 import * as Security from './Security';
@@ -18,6 +17,6 @@ import * as Mocks from '../test/Mocks';
 
 export {
     Authentication, ComplexTypes, FieldSettings, Schemas, Enums,
-    ContentTypes, Collection, ODataHelper, ODataAPIActionObservables, ODataApi,
+    ContentTypes, Collection, ODataHelper, ODataApi,
     Resources, Security, HttpProviders, Repository, Config, Mocks
 };
