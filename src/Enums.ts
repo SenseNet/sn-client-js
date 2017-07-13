f159764d1891c6f89844fbfa79bbc5acce96ceaa
gallayl <gallay.lajos@gmail.com>
2017-07-13 16:07:32 +0200

    feat(Enums): Updated Enums to Typescript 2.4.1 string based enums

--- src/Enums.ts
+++ src/Enums.ts
@@ -18,18 +18,82 @@
  * ```
  */ /** */
 
-    export enum VersioningMode { Option0, Option1, Option2, Option3 }
-    export enum InheritableVersioningMode { Option0, Option1, Option2, Option3 }
-    export enum ApprovingMode { Option0, Option1, Option2 }
-    export enum InheritableApprovingMode { Option0, Option1, Option2 }
-    export enum SavingState { Finalized, Creating, Modifying, ModifyingLocked }
-    export enum GroupAttachments { email, root, subject, sender }
-    export enum EnableAutofilters { Default, Enabled, Disabled }
-    export enum EnableLifespanFilter { Default, Enabled, Disabled }
-    export enum Language { En, Hu }
-    export enum MemoType { generic, iso, iaudit }
-    export enum Priority { Option0, Option1, Option2 }
-    export enum Status { pending, active, completed, deferred, waiting }
-    export enum QueryType { Public, Private }
-    export enum Gender { Option0, Female, Male }
-    export enum MaritalStatus { Option0, Single, Married }
+export enum VersioningMode {
+     Option0 = '0',
+     Option1 = '1',
+     Option2 = '2',
+     Option3 = '3',
+}
+export enum InheritableVersioningMode {
+     Option0 = '0',
+     Option1 = '1',
+     Option2 = '2',
+     Option3 = '3',
+}
+export enum ApprovingMode {
+     Option0 = '0',
+     Option1 = '1',
+     Option2 = '2',
+}
+export enum InheritableApprovingMode {
+     Option0 = '0',
+     Option1 = '1',
+     Option2 = '2',
+}
+export enum SavingState {
+     Finalized = '0',
+     Creating = '1',
+     Modifying = '2',
+     ModifyingLocked = '3',
+}
+export enum GroupAttachments {
+     email = 'email',
+     root = 'root',
+     subject = 'subject',
+     sender = 'sender',
+}
+export enum EnableAutofilters {
+     Default = '0',
+     Enabled = '1',
+     Disabled = '2',
+}
+export enum EnableLifespanFilter {
+     Default = '0',
+     Enabled = '1',
+     Disabled = '2',
+}
+export enum Language {
+     En = 'en',
+     Hu = 'hu',
+}
+export enum MemoType {
+     generic = 'generic',
+     iso = 'iso',
+     iaudit = 'iaudit',
+}
+export enum Priority {
+     Option0 = '1',
+     Option1 = '2',
+     Option2 = '3',
+}
+export enum Status {
+     pending = 'pending',
+     active = 'active',
+     completed = 'completed',
+     deferred = 'deferred',
+     waiting = 'waiting',
+}
+export enum QueryType {
+     Public = 'Public',
+     Private = 'Private',
+}
+export enum Gender {
+     Option0 = '...',
+     Female = 'Female',
+     Male = 'Male',
+}
+export enum MaritalStatus {
+     Option0 = '...',
+     Single = 'Single',
+     Married = 'Married',
+}
