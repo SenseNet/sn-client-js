fcf770e41f989b0fc5840c2246136cc603b62e10
gallayl <gallay.lajos@gmail.com>
2017-09-19 12:59:32 +0200

    [KFI]feat(ODataParams): Extended 'orderby' parameter with an optional asc/desc parameter

--- src/ODataApi/ODataParams.ts
+++ src/ODataApi/ODataParams.ts
@@ -5,6 +5,7 @@
 import { Content } from '../Content';
 
 export type ODataFieldParameter<T extends Content> = (keyof T['options'])[] | keyof T['options'];
+export type ODataOrderParameter<T extends Content> = keyof T['options'] | (keyof T['options'] | [keyof T['options'], 'asc' | 'desc'])[];
 export type ODataMetadataType = 'full' | 'minimal' | 'no';
 
 export type ODataFormatType = 'json' | 'verbosejson';
@@ -12,7 +13,7 @@ export type ODataFormatType = 'json' | 'verbosejson';
 export class IODataParams<T extends Content> {
     select?: ODataFieldParameter<T> | 'all';
     expand?: ODataFieldParameter<T>;
-    orderby?: ODataFieldParameter<T>;
+    orderby?: ODataOrderParameter<T>;
     top?: number;
     skip?: number;
     filter?: string;
