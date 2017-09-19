7e4c618664754f004e36daca97dce0f7fac51f47
gallayl <gallay.lajos@gmail.com>
2017-09-19 13:17:11 +0200

    [KFI]docs(ODataParams): added typedocs for ODataParams and usage example for OrderBy

--- src/ODataApi/ODataParams.ts
+++ src/ODataApi/ODataParams.ts
@@ -6,21 +6,85 @@ import { Content } from '../Content';
 
 export type ODataFieldParameter<T extends Content> = (keyof T['options'])[] | keyof T['options'];
 export type ODataOrderParameter<T extends Content> = keyof T['options'] | (keyof T['options'] | [keyof T['options'], 'asc' | 'desc'])[];
+
 export type ODataMetadataType = 'full' | 'minimal' | 'no';
-
 export type ODataFormatType = 'json' | 'verbosejson';
+export type ODataInlineCountType = 'none' | 'allpages';
 
+/**
+ * Model class to define specific OData Request parameters. See http://wiki.sensenet.com/OData_REST_API
+ */
 export class IODataParams<T extends Content> {
+    /**
+     * The field(s) to be include in a $select list. Can be a field (e.g. 'DisplayName'), an array of fields (e.g. ['Name', 'Type']) or 'all'
+     */
     select?: ODataFieldParameter<T> | 'all';
+    /**
+     * The field(s) to be include in an $expand list. Can be a reference field (e.g. 'Owner') or an array of fields (e.g. ['CreatedBy', 'ModifiecBy'])
+     */
     expand?: ODataFieldParameter<T>;
+
+    /**
+     * Sets the OData $orderby parameter. Usage example
+     * ```ts
+     * // simple field
+     * {
+     *    ...
+     *    orderby: 'Name'
+     * }
+     * // list with fields or tuples with order direction
+     * {
+     *    ...
+     *    orderby: [
+     *      ['CreationDate', 'desc']
+     *      'Name',
+     *      'DisplayName'
+     *    ]
+     *    
+     * }
+     * 
+     * ```
+     */
     orderby?: ODataOrderParameter<T>;
+    /**
+     * Sets the OData $top parameter
+     */
     top?: number;
+
+    /**
+     * Sets the OData $skip parameter
+     */
     skip?: number;
+
+    /**
+     * Sets the OData $filter parameter
+     */
     filter?: string;
+
+    /**
+     * Sets the OData $format parameter. Can be 'json' or 'verbosejson'
+     */
     format?: ODataFormatType;
-    inlinecount?: string;
+
+    /**
+     * Sets the OData $format parameter. Can be 'json' or 'verbosejson'
+     */
+    inlinecount?: ODataInlineCountType;
+
+    /**
+     * Sets the OData 'query' parameter. Can be a Content Query
+     */
     query?: string;
+    /**
+     * Sets the OData metadata parameter. Can be 'full', 'minimal' or 'no'
+     */
     metadata?: ODataMetadataType;
+    /**
+     * Sets the OData post data object
+     */
     data?: Object;
+    /**
+     * Sets the OData Scenario parameter
+     */
     scenario?: string;
 }
