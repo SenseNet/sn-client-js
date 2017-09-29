43362ba1c25d2c6715bf402223ec281bbd7ac6f1
gallayl <gallay.lajos@gmail.com>
2017-09-29 16:51:41 +0200

    [KFI]docs(Query): added Codepen example snippet

--- src/Query/index.ts
+++ src/Query/index.ts
@@ -3,6 +3,8 @@
  * @preferred
  * 
  * @description Classes and Methods for creating, manipulating and executing content queries in sensenet ECM.
+ * <p data-height="339" data-theme-id="light" data-slug-hash="xXrLXw" data-default-tab="js,result" data-user="gallayl" data-embed-version="2" data-pen-title="@sensenet/sn-client-js/query" class="codepen">See the Pen <a href="https://codepen.io/gallayl/pen/xXrLXw/">@sensenet/sn-client-js/query</a> by gallayl (<a href="https://codepen.io/gallayl">@gallayl</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+<script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
  * */ /** */
  
 export * from './Query'
