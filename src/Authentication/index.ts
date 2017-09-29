64a648600db74b9778b7cc0b6b649e430cf4442a
gallayl <gallay.lajos@gmail.com>
2017-09-29 16:18:41 +0200

    [KFI]docs(Authentication): added CodePen example snippet

--- src/Authentication/index.ts
+++ src/Authentication/index.ts
@@ -3,7 +3,7 @@
  * @preferred
  * @description This module that contains authentication-related classes, types and interfaces
  * 
- * <p data-height="332" data-theme-id="light" data-slug-hash="qPaXRM" data-default-tab="js,result" data-user="gallayl" data-embed-version="2" data-pen-title="@sensenet/sn-client-js/login" class="codepen">See the Pen <a href="https://codepen.io/gallayl/pen/qPaXRM/">@sensenet/sn-client-js/login</a> by Lajos (<a href="https://codepen.io/gallayl">@gallayl</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+ * <p data-height="268" data-theme-id="light" data-slug-hash="qPjmdj" data-default-tab="result" data-user="gallayl" data-embed-version="2" data-pen-title="@sensenet/sn-client-js/login" class="codepen">See the Pen <a href="https://codepen.io/gallayl/pen/qPjmdj/">@sensenet/sn-client-js/login</a> by gallayl (<a href="https://codepen.io/gallayl">@gallayl</a>) on <a href="https://codepen.io">CodePen</a>.</p>
 <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
  */ /** */
  
