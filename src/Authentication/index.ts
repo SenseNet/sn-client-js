405cdaccfb3cf69d6bf7adb1e71a56e7443843f1
gallayl <gallay.lajos@gmail.com>
2017-09-29 14:34:22 +0200

    [KFI]docs(Authentication): Codepen snippet for Authentication module

--- src/Authentication/index.ts
+++ src/Authentication/index.ts
@@ -2,6 +2,9 @@
  * @module Authentication
  * @preferred
  * @description This module that contains authentication-related classes, types and interfaces
+ * 
+ * <p data-height="332" data-theme-id="light" data-slug-hash="qPaXRM" data-default-tab="js,result" data-user="gallayl" data-embed-version="2" data-pen-title="@sensenet/sn-client-js/login" class="codepen">See the Pen <a href="https://codepen.io/gallayl/pen/qPaXRM/">@sensenet/sn-client-js/login</a> by Lajos (<a href="https://codepen.io/gallayl">@gallayl</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+<script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
  */ /** */
  
 export * from './IAuthenticationService';
