a1e6f7060cceb211a5fcfce48362aef60748fe61
gallayl <gallay.lajos@gmail.com>
2017-10-13 16:28:16 +0200

    [KFI]test(RxHttpProvider): added Upload unit test

--- test/HttpProviderTests.ts
+++ test/HttpProviderTests.ts
@@ -9,45 +9,57 @@ const expect = Chai.expect;
 @suite('BaseHttpProvider')
 export class HttpProviderTests {
 
-    private readonly testHeaderName: string = 'testHeader';
-    private readonly testHeaderValue: string = 'testHeaderValue';
+    private readonly _testHeaderName: string = 'testHeader';
+    private readonly _testHeaderValue: string = 'testHeaderValue';
 
     @test
     public setGlobalHeaders(){
         let p = new MockHttpProvider();
-        p.SetGlobalHeader(this.testHeaderName, this.testHeaderValue);
+        p.SetGlobalHeader(this._testHeaderName, this._testHeaderValue);
         let headers = p.actualHeaders;
-        Chai.expect(headers[this.testHeaderName as any]).to.be.eq(this.testHeaderValue);
+        Chai.expect(headers[this._testHeaderName as any]).to.be.eq(this._testHeaderValue);
     }
 
     @test
     public unsetGlobalHeaders(){
         let p = new MockHttpProvider();
-        p.SetGlobalHeader(this.testHeaderName, this.testHeaderValue);
+        p.SetGlobalHeader(this._testHeaderName, this._testHeaderValue);
         let headers = p.actualHeaders;
-        Chai.expect(headers[this.testHeaderName as any]).to.be.eq(this.testHeaderValue);
+        Chai.expect(headers[this._testHeaderName as any]).to.be.eq(this._testHeaderValue);
 
-        p.UnsetGlobalHeader(this.testHeaderName);
+        p.UnsetGlobalHeader(this._testHeaderName);
         headers = p.actualHeaders;
-        Chai.expect(headers[this.testHeaderName as any]).to.be.eq(undefined);
-    }    
+        Chai.expect(headers[this._testHeaderName as any]).to.be.eq(undefined);
+    }
 
     @test
     public 'globalHeaders should override options.headers'() {
         let p = new MockHttpProvider();
-        p.SetGlobalHeader(this.testHeaderName, this.testHeaderValue);
+        p.SetGlobalHeader(this._testHeaderName, this._testHeaderValue);
 
         let options: any = {};
-        options[this.testHeaderName as any] = 'modifiedValue';
+        options[this._testHeaderName as any] = 'modifiedValue';
         p.Ajax(Object, options).toPromise();
-        expect((p.lastOptions as any).headers[this.testHeaderName as any]).to.be.eq(this.testHeaderValue);
+        expect((p.lastOptions as any).headers[this._testHeaderName as any]).to.be.eq(this._testHeaderValue);
     }
 
     @test
-    public 'RxHttpProvider should return an Observable<TReturns>'() {
-        (global as any).XMLHttpRequest = class { open(){}; send(){}; setRequestHeader(){}};
+    public 'RxHttpProvider Ajax should return an Observable<TReturns>'() {
         let p = new RxAjaxHttpProvider();
         let obs = p.Ajax(Object, {});
+        expect(obs).to.be.instanceof(Observable);
+    }
+
+    @test
+    public 'RxHttpProvider Upload should return an Observable<TReturns>'(){
+        (global as any).XMLHttpRequest = class { open(){}; send(){}; setRequestHeader(){}};
+        (global as any).File = class { };
+        (global as any).FormData = class { append(){}; };
+        let p = new RxAjaxHttpProvider();
+        let file = new File(['alma'], 'alma.txt');
+        let obs = p.Upload(Object, file, {
+            url: '',
+        });
         expect(obs).to.be.instanceof(Observable);
     }
 
