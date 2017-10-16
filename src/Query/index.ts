5e98376a0eb18fe37fb2abf39897329ca5999bba
gallayl <gallay.lajos@gmail.com>
2017-10-16 15:32:41 +0200

    [KFI]chore(Query): lint

--- src/Query/index.ts
+++ src/Query/index.ts
@@ -1,10 +1,10 @@
 /**
  * @module Query
  * @preferred
- * 
+ *
  * @description Classes and Methods for creating, manipulating and executing content queries in sensenet ECM.
  * */ /** */
- 
+
 export * from './Query'
 export * from './QueryResult'
 export * from './QuerySegment'
